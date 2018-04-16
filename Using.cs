5596d45051bd9ec9a8acf10d0bffcac171693f1d
bhargavak <bhargavak37@gmail.com>
2018-04-16 14:49:07 +0530

    Examples "Using" as a Statement

--- Using.cs
+++ Using.cs
@@ -10,17 +10,38 @@ using System.Math;                 without having to qualify the                
 Examples "Using" as a Statement
 IDisposable
        IDisposable is an interface that provides a mechanism to release unmanaged resources
-  
+                                                                            Dispose resources with using
   Quick IDisposible Example
-  public class BooksLibrary : IDisposable
+  public class BooksLibrary : IDisposable                                   
+  {                                                                       using (BooksLibrary currentLibrary = new                                                                                  
+  public List<Book> Library { get; set;}                                  BooksLibrary())
+  public void Dispose()                                                   {
+  {                                                                        foreach (var book in currentLibrary.Library)
+  if (Library != null)                                                     {
+  {                                                                           if (book.Title == title)
+  Library.Clear();                                                            {
+  Library = null;                                                               return book;  
+  }                                                                           }
+  }                                                                         }
+  }                                                                         return null;
+Dispose resources without using                                           }
+  BooksLibrary currentLibrary = null;                                     
+try
+{
+  currentLibrary = new BooksLibrary();
+  foreach (var book in currentLibrary.Library)
   {
-  public List<Book> Library { get; set;}
-  public void Dispose()
-  {
-  if (Library != null)
-  {
-  Library.Clear();
-  Library = null;
+    if (book.Title == title) return book;
   }
-  }
-  }
+  return null;
+}
+catch (Exception)
+{
+  return null; //Handle Exception
+} 
+finally
+{
+if (currentLibrary != null)
+  currentLibrary.Dispose(); 
+}
+
